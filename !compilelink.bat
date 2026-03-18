88a67681aa255dd8c827e8f46488a7c8cc98385f
Nikita Zimin <nzeemin@gmail.com>
2026-03-18 18:45:26 +0300

    Switched to macro11/pclink1 toolchain, encoding changed to KOI8-R

--- !compilelink.bat
+++ !compilelink.bat
@@ -1,5 +1,4 @@
 @echo off
-set rt11exe=C:\bin\rt11\rt11.exe
 
 rem Define ESCchar to use in ANSI escape sequences
 rem https://stackoverflow.com/questions/2048509/how-to-echo-with-different-colors-in-the-windows-command-line
@@ -10,7 +9,6 @@ set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,
 set "DATESTAMP=%YYYY%-%MM%-%DD%"
 for /f %%i in ('git rev-list HEAD --count') do (set REVISION=%%i)
 echo V%REVISION% %DATESTAMP%
-
 echo VERSTR:	.ASCII "V%REVISION% %DATESTAMP%" > VERSIO.MAC
 
 @if exist ROBIN.LST del ROBIN.LST
@@ -21,29 +19,30 @@ echo LEVELS:: .BLKW 42. >> LEVELS.MAC
 
 set /a scrno = 1
 :loopscrno
-if "%scrno%" == "5" goto loopend
+if "%scrno%" == "8" goto loopend
   echo LEVEL %scrno%
   copy /b LEVEL%scrno%.MAC LEVEL.MAC >NUL
-  %rt11exe% MACRO/LIST:DK:ROBIN%scrno% ROBIN.MAC /OBJECT:ROBIN%scrno%
-  for /f "delims=" %%a in ('findstr /B "Errors detected" ROBIN%scrno%.LST') do set "errdet=%%a"
-  if "%errdet%"=="Errors detected:  0" (
-    echo COMPILED SUCCESSFULLY
+  REM %rt11exe% MACRO/LIST:DK:ROBIN%scrno% ROBIN.MAC /OBJECT:ROBIN%scrno%
+  tools\macro11.exe ROBIN.MAC -l ROBIN%scrno%.lst -o ROBIN%scrno%.obj -rt11 -se -m SYSMAC.SML
+  if not errorlevel 1 (
+    echo LEVEL %scrno% COMPILED SUCCESSFULLY
   ) ELSE (
-    findstr /RC:"^[ABDEILMNOPQRTUZ] " ROBIN%scrno%.LST
+    findstr /RC:"^[ABDEILMNOPQRTUZ] " S1COMM.lst
     echo ======= %errdet% =======
-    exit /b
+    goto :Failed
   )
   del LEVEL.MAC
-  %rt11exe% LINK ROBIN%scrno% /MAP:ROBIN%scrno%.MAP
-  for /f "delims=" %%a in ('findstr /B "Undefined globals" ROBIN%scrno%.MAP') do set "undefg=%%a"
-  if "%undefg%"=="" (
-    rem type ROBIN%scrno%.MAP
-    echo.
-    echo LINKED SUCCESSFULLY
-  ) ELSE (
-    echo %ESCchar%[91m======= LINK FAILED =======%ESCchar%[0m
-    exit /b
+  tools\pclink11.exe ROBIN%scrno%.OBJ /MAP /VERBOSITY:1
+  if errorlevel 1 (
+    echo ======= LINK FAILED =======
+    goto :Failed
   )
+  for /f "delims=" %%a in ('findstr /B "Undefined globals" ROBIN%scrno%.MAP') do set "undefg=%%a"
+  if not "%undefg%"=="" (
+    echo ======= LINK FAILED: Undefined globals =======
+    goto :Failed
+  )
+  echo LEVEL %scrno% LINKED SUCCESSFULLY
   del ROBIN%scrno%.OBJ
   rem del ROBIN%scrno%.LST
   set /a scrno += 1
@@ -52,30 +51,36 @@ goto loopscrno
 
 .\PrepareRobinDat\bin\Debug\net7.0\PrepareRobinDat.exe
 
-echo. > LEVEL.MAC
-%rt11exe% MACRO/LIST:DK: ROBIN.MAC
-for /f "delims=" %%a in ('findstr /B "Errors detected" ROBIN.LST') do set "errdet=%%a"
-if "%errdet%"=="Errors detected:  0" (
-  echo COMPILED SUCCESSFULLY
+echo. ESTRAT = .+2 > LEVEL.MAC
+tools\macro11.exe ROBIN.MAC -l ROBIN.lst -o ROBIN.obj -rt11 -se -m SYSMAC.SML
+if not errorlevel 1 (
+  echo ROBIN COMPILED SUCCESSFULLY
 ) ELSE (
-  findstr /RC:"^[ABDEILMNOPQRTUZ] " ROBIN.LST
+  findstr /RC:"^[ABDEILMNOPQRTUZ] " S1COMM.lst
   echo ======= %errdet% =======
-  exit /b
+  goto :Failed
 )
 del LEVEL.MAC
 
 @if exist ROBIN.MAP del ROBIN.MAP
 @if exist ROBIN.SAV del ROBIN.SAV
 
-%rt11exe% LINK ROBIN /MAP:ROBIN.MAP
-for /f "delims=" %%a in ('findstr /B "Undefined globals" ROBIN.MAP') do set "undefg=%%a"
-if "%undefg%"=="" (
-  rem type ROBIN.MAP
-  echo.
-  echo %ESCchar%[92mLINKED SUCCESSFULLY%ESCchar%[0m
-) ELSE (
-  echo %ESCchar%[91m======= LINK FAILED =======%ESCchar%[0m
-  exit /b
+tools\pclink11.exe ROBIN.OBJ /MAP /VERBOSITY:1
+if errorlevel 1 (
+  echo ======= LINK FAILED =======
+  goto :Failed
 )
+for /f "delims=" %%a in ('findstr /B "Undefined globals" ROBIN.MAP') do set "undefg=%%a"
+if not "%undefg%"=="" (
+  echo ======= LINK FAILED: Undefined globals =======
+  goto :Failed
+)
+echo LINKED SUCCESSFULLY
 
 echo %ESCchar%[92mDONE%ESCchar%[0m
+exit
+
+:Failed
+@echo off
+echo %ESCchar%[91mFAILED%ESCchar%[0m
+exit /b
